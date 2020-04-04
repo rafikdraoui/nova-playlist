408ec35b8ec7d8adab5b109d39fc069f8d7740bf
Rafik Draoui <rafik@rafik.ca>
2020-04-04 10:25:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# nova-playlist
+
+[Radio Nova][0] plays amazing music at night.
+
+There could be Fela Kuti, then hip-hop from the 1990s, then Motown, then Tuareg
+music, then some minimal electro, then Hungarian folk songs, then David Bowie,
+then Amazonian cumbia, then a French song that was released last month, then
+Sufi devotional music from Punjab, then Nina Simone, then a tune from the
+north-east of Brazil. It's wild.
+
+And if you, like me, live a few time zones west of Paris, you get to
+conveniently enjoy it all in the evening instead of having to be up in the
+middle of the night!
+
+Often, I want to know which song is currently playing, so that I can find out
+more about it and the artist. There is a [page][1] where one can find what
+music has aired on the Radio in the past, but it's buggy and a bit annoying to
+use. And even if the page was great to use, it wouldn't be as great as using
+the command line for it! _Ergo_ this little script.
+
+
+## Installation
+
+    pipx install nova-playlist
+
+Or use `pip`.
+
+
+## Usage
+
+    $ nova
+    20:36  Orchestra Baobab - Liiti Liiti
+    20:30  Chico Buarque - Construção
+    20:27  Abner Jay - My Middle Name Is The Blues
+    20:23  Brigitte Fontaine - Le Goudron
+    ...
+
+By default, the times are in the "US/Eastern" time zone. If you want them in
+another time zone, you can pass the `-t`/`--timezone` argument:
+
+    $ nova -t Europe/Berlin
+    03:12  Los Camperos De Valles - El Gallo
+    03:09  Karen Dalton - Something On Your Mind
+    03:04  Têtes Raides & Yann Tiersen - Ginette
+    02:59  The Toraia Orchestra Of Algiers - Nar Houbi Techaal
+    ...
+
+
+## Caveat
+
+This can be somewhat brittle. Sometimes, the data stops being updated on the
+site. Or the data gets updated, but the timestamps are 11 minutes off from
+what's playing in the stream. And of course, if they ever change their HTML
+structure in any way, this could also start failing miserably. But so far, so
+good!
+
+
+[0]: https://www.nova.fr/radionova/nova-la-nuit
+[1]: https://www.nova.fr/radionova/radio-nova
---
+++ nova_playlist.py
@@ -0,0 +1,75 @@
+import argparse
+from datetime import datetime, timedelta
+from urllib.request import urlopen
+
+import pytz
+from bs4 import BeautifulSoup
+
+playlist_url = "https://www.nova.fr/radionova/radio-nova"
+paris_time = datetime.now(tz=pytz.timezone("Europe/Paris"))
+
+
+def main():
+    args = parse_args()
+    playlist = get_playlist()
+    for song in playlist:
+        song["time"] = localize(args.timezone, song["time"], args.offset)
+        print("{time}  {artist} - {title}".format(**song))
+
+
+def to_tz(tz_name):
+    if tz_name not in pytz.all_timezones:
+        raise argparse.ArgumentTypeError(f"{tz_name} is not a valid time zone")
+    return pytz.timezone(tz_name)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Display information about the songs that recently played on Radio Nova"
+    )
+    parser.add_argument(
+        "-t",
+        "--timezone",
+        default="US/Eastern",
+        type=to_tz,
+        help="Local time zone to display the song times in (default: US/Eastern)",
+    )
+    parser.add_argument(
+        "-o",
+        "--offset",
+        default=0,
+        type=int,
+        help="Number of minutes that song times need to be shifted by (default: 0)",
+    )
+    return parser.parse_args()
+
+
+def get_playlist():
+
+    # The `data` kwarg is needed to force a POST request. Sending an empty body
+    # will retrieve the latest information, i.e no need to provide `day`,
+    # `month`, `hour`, etc. in the form data.
+    resp = urlopen(playlist_url, data=b"")
+    html = BeautifulSoup(resp.read(), "html.parser")
+    items = html.find_all("div", "square-item")
+    return [parse_song(item) for item in items]
+
+
+def parse_song(item):
+    artist = item.find("div", "name").text.title()
+    title = item.find("div", "description").text.title()
+
+    # The `<time>` element has a convenient `datetime` attribute, but sadly
+    # it's always set to "2012-02-11T16:24:02", so we need to read its text
+    # instead and compute the timestamp ourselves.
+    time = item.find("time").text
+
+    return {"artist": artist, "title": title, "time": time}
+
+
+def localize(local_tz, time, offset=0):
+    hour, minute = time.split(":")
+    paris_song_time = paris_time.replace(hour=int(hour), minute=int(minute))
+    local_song_time = paris_song_time.astimezone(local_tz)
+    display_time = local_song_time + timedelta(minutes=offset)
+    return display_time.strftime("%H:%M")
